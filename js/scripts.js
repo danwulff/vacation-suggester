b6ebaef2ed818d311b8004ec2f1f7ad91362fe89
Daniel Wulff <danielwulff.ee@gmail.com>
2016-03-25 09:57:00 -0700

    created most form entries in index.html

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+//returns true or false depending on wheter valid email
+function validateEmail(email) {
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
+
+$(document).ready(function() {
+
+});
